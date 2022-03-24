e47dd512278a7369be2a609e81ba44e4e5b93e15
cdugger <calvind@email.arizona.edu>
2022-03-23 21:54:27 -0700

    Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import WeekView from "./components/WeekView";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
-      <WeekView />
+      <Header />
+      {/* <WeekView /> */}
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+import Navbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import '../css/Header.css';
+
+
+const Header = () => {
+    return (
+        <Navbar className="py-4">
+            <Container fluid>
+                <Navbar.Brand className="fs-2" href="#"><span class="text-warning">University</span><span className="text-light">Access</span></Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#">Home</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
